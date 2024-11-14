97f9c61a5d4c616dfc750c0f26c301c12082cf97
masha-maltseva <145387904+masha-maltseva@users.noreply.github.com>
2024-11-14 15:38:08 -0500

    add try catch

--- app/(admin)/(home)/page.tsx
+++ app/(admin)/(home)/page.tsx
@@ -2,6 +2,13 @@ import KeysList from "./keys-list";
 import { getAllApiKeys } from "./services/get-all-api-keys";
 
 export default async function HomePage() {
-  const result = await getAllApiKeys();
+  let result = [];
+
+  try {
+    result = await getAllApiKeys();
+  } catch (error) {
+    console.error("Failed to get the list of api keys", error);
+  }
+
   return <KeysList keys={result} />;
 }
