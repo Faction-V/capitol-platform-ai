3d184a7c552e54ef639a5e6feb4414e666a60709
masha-maltseva <145387904+masha-maltseva@users.noreply.github.com>
2024-11-21 12:46:00 -0500

    fix name

--- app/(admin)/members/member-item.tsx
+++ app/(admin)/members/member-item.tsx
@@ -5,6 +5,7 @@ import { Member } from "./types";
 import { DeleteConfirmationModal } from "../../components/delete-confirmation-modal";
 import { toast } from "react-toastify";
 import { removeMember } from "./service/remove-member";
+import { isStringEmpty } from "../../utils/is-string-empty";
 
 const colors: { [index: string]: string } = {
   a: "bg-red-300 text-red-950",
@@ -46,7 +47,9 @@ export const MemberItem = ({
   id,
   updateMembersAfterRemove,
 }: MemberItemProps) => {
-  const [firstName, lastName] = fullName.trim().split(/\s+/);
+  const name = isStringEmpty(fullName.trim()) ? "Unknown name" : fullName;
+
+  const [firstName, lastName] = name.trim().split(/\s+/);
   const [isConfirmationModalOpen, setIsConfirmationModalOpen] = useState(false);
 
   const colorClassname = colors[lastName[0].toLowerCase()];
