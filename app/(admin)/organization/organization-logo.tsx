94d66425ba9495a8a545d292bdafb9a8c9deb941
masha-maltseva <145387904+masha-maltseva@users.noreply.github.com>
2024-12-03 17:15:25 -0500

    display logo and name

--- app/(admin)/organization/organization-logo.tsx
+++ app/(admin)/organization/organization-logo.tsx
@@ -6,8 +6,18 @@ import { Dropzone } from "../../components/dropzone";
 import { Button } from "../../components/button";
 import { EditIcon } from "../../icons/edit-icon";
 
-export const OrganizationLogo = () => {
+interface OrganizationLogoProps {
+  logoUrl: string;
+}
+
+export const OrganizationLogo = ({ logoUrl }: OrganizationLogoProps) => {
   const [isEditMode, setIsEditMode] = useState(false);
+  const [image, setImage] = useState<string>(logoUrl);
+
+  const handleUpload = (imageUrl: string) => {
+    setImage(imageUrl);
+    setIsEditMode(false);
+  };
 
   return (
     <div className="flex gap-2 flex-col bg-white border border-gray-200 rounded-lg shadow justify-between items-start p-5 mb-2">
@@ -19,7 +29,11 @@ export const OrganizationLogo = () => {
           type="secondary"
         />
       </div>
-      {isEditMode ? <Dropzone /> : "image"}
+      {isEditMode ? (
+        <Dropzone handleUpload={handleUpload} />
+      ) : (
+        <img src={image} alt="" className="h-10 w-10 object-cover rounded-md" />
+      )}
     </div>
   );
 };
--- app/components/dropzone.tsx
+++ app/components/dropzone.tsx
@@ -1,23 +1,20 @@
 "use client";
 
-import { useState } from "react";
 import { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
 import { ImageIcon } from "@/app/icons/image-icon";
 import { updateOrgLogo } from "../(admin)/organization/services/upload-org-logo";
 
-export const Dropzone = () => {
-  const [image, setImage] = useState<string | null>(null);
-  const onDrop = useCallback(() => {
-    // Do something with the files
-  }, []);
+interface DropzoneProps {
+  handleUpload: (image: string) => void;
+}
 
+export const Dropzone = ({ handleUpload }: DropzoneProps) => {
   const onDropAccepted = useCallback(async (acceptedFiles: File[]) => {
     const result = await updateOrgLogo({ file: acceptedFiles[0] });
-    setImage(result?.url);
+    handleUpload(result?.url);
   }, []);
   const { getRootProps, getInputProps } = useDropzone({
-    onDrop,
     onDropAccepted,
   });
 
@@ -27,21 +24,16 @@ export const Dropzone = () => {
       className="border rounded-md border-dashed bg-neutral-100 border-neutral-300 w-full py-5"
     >
       <input {...getInputProps()} />
-      {/*{isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      )}*/}
       <div className="flex flex-col gap-1 items-center justify-center">
         <ImageIcon size={24} />
         <div className="flex flex-col gap">
           <span className="text-center font-semibold">Drop an image here</span>
+          <span className="text-center">40 x 40px</span>
           <span className="text-center text-neutral-500">
             or click to browse
           </span>
         </div>
       </div>
-      {image && <img src={image} alt="Preview" />}
     </div>
   );
 };
