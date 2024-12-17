7c5c0581da4254f4bb60a9a001c1ebcbddeabf6a
masha-maltseva <145387904+masha-maltseva@users.noreply.github.com>
2024-12-17 15:16:37 -0500

    add field for guardrails

--- app/components/checkbox.tsx
+++ app/components/checkbox.tsx
@@ -12,7 +12,7 @@ export const Checkbox = ({ label, id, isChecked, onClick }: CheckboxProps) => {
         id={id}
         type="checkbox"
         value=""
-        checked={isChecked}
+        defaultChecked={isChecked}
         onClick={onClick}
         className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
       />
